2a191c0f4799527f702acbd68d778d5933a23d68
SangWoon123 <alrax3574@naver.com>
2024-05-30 19:29:18 +0900

    Update WorldCupReady.kt

--- app/src/main/java/com/example/myapplication/worldcup/WorldCupReady.kt
+++ app/src/main/java/com/example/myapplication/worldcup/WorldCupReady.kt
@@ -1,9 +1,7 @@
 package com.example.myapplication.worldcup
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityWorldCupReadyBinding
 
